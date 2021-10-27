3eaf7feb92ad23bb81d7f64c16942c12cceb53af
dcrocker <danielacrocker@gmail.com>
2021-10-27 09:18:13 -0400

    deadlock version - consumer can hold mutex

--- prodcon3/main.cpp
+++ prodcon3/main.cpp
@@ -25,23 +25,28 @@ int loops = 0;
 
 sem_t empty;
 sem_t full;
+sem_t mutex;
 
 void *producer(void *arg)
 {
     int i;
     for (i = 0; i < loops; i++) {
+        sem_wait(&mutex);
         sem_wait(&empty);
         put(i);
         sem_post(&full);
+        sem_post(&mutex);
     }
 }
 
 void *consumer(void *arg) {
     int i;
     for (i = 0; i  < loops; i++) {
+        sem_wait(&mutex);
         sem_wait(&full);
         int b = get();
         sem_post(&empty);
+        sem_post(&mutex);
         printf("%d\n", b);
     }
 }
@@ -57,6 +62,7 @@ int main(int argc, char *argv[])
 
     sem_init(&empty,0, MAX);
     sem_init(&full, 0, 0);
+    sem_init(&mutex,0, 1);
 
     pthread_t pThread, cThread;
     pthread_create(&pThread, 0, producer, 0);
