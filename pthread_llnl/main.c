30f53cbb50a23eb52132aca41e6470bb867d0278
dcrocker <danielacrocker@gmail.com>
2021-10-29 16:21:20 -0400

    params stored in struct and passed to thread

--- pthread_llnl/main.c
+++ pthread_llnl/main.c
@@ -4,16 +4,28 @@
 
 const int NUM_THREADS = 8;
 
+struct thread_data {
+    int thread_id;
+    int sum;
+    char *message;
+};
+
+struct thread_data thread_data_array[8];
+
 char *messages[8];
 
-void *PrintHello(void *threadid)
+void *PrintHello(void *threadarg)
 {
-    long taskid;
+    int taskid, sum;
+    char *hello_msg;
+    struct thread_data *my_data;
 
     sleep(1);
-    taskid = (long) threadid;
-
-    printf("Thread %d: %s\n", taskid, messages[taskid]);
+    my_data = (struct thread_data *) threadarg;
+    taskid = my_data->thread_id;
+    sum = my_data->sum;
+    hello_msg = my_data->message;
+    printf("Thread %d: %s Sum=%d\n", taskid, hello_msg, sum);
     pthread_exit(NULL);
 }
 
@@ -21,8 +33,9 @@ int main(int argc, char *argv[])
 {
     pthread_t threads[NUM_THREADS];
     long taskids[NUM_THREADS ];
-    int rc, t;
+    int rc, t, sum;
 
+    sum = 0;
     messages[0] = "English: Hello World!";
     messages[1] = "French: Bonjour le monde!";
     messages[2] = "Spanish: Hola al mundo!";
@@ -33,11 +46,14 @@ int main(int argc, char *argv[])
     messages[7] = "Latin: Orbis, te saluto!";
 
     for(t = 0; t < NUM_THREADS; t++) {
-        taskids[t] = t;
+        sum += t;
+        thread_data_array[t].thread_id = t;
+        thread_data_array[t].sum = sum;
+        thread_data_array[t].message = messages[t];
 
         printf("Creating thread %ld\n", t);
 
-        rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+        rc = pthread_create(&threads[t], NULL, PrintHello, (void *)&thread_data_array[t]);
 
         if (rc) {
             printf("ERROR, return code from pthread_create() is %d\n", rc);
