871a79bce197bbb33d20f8a8def9419eebb99f98
dcrocker <danielacrocker@gmail.com>
2021-10-29 16:03:32 -0400

    passing arg(s) to thread

--- pthread_llnl/main.c
+++ pthread_llnl/main.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-const int NUM_THREADS = 5;
+const int NUM_THREADS = 8;
+
+char *messages[8];
 
 void *PrintHello(void *threadid)
 {
-    long tid;
-    tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
+    long taskid;
+
+    sleep(1);
+    taskid = (long) threadid;
+
+    printf("Thread %d: %s\n", taskid, messages[taskid]);
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
     pthread_t threads[NUM_THREADS];
-    int rc;
-    long t;
+    long taskids[NUM_THREADS ];
+    int rc, t;
+
+    messages[0] = "English: Hello World!";
+    messages[1] = "French: Bonjour le monde!";
+    messages[2] = "Spanish: Hola al mundo!";
+    messages[3] = "Klingon: Nuq neH!";
+    messages[4] = "German: Guten Tag, welt!";
+    messages[5] = "Russian: Zdravstvuyte, mir!";
+    messages[6] = "Japan: Sekai e konnichiwa!";
+    messages[7] = "Latin: Orbis, te saluto!";
+
     for(t = 0; t < NUM_THREADS; t++) {
-        printf("In main: creating thread %ld\n", t);
+        taskids[t] = t;
+
+        printf("Creating thread %ld\n", t);
+
         rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
 
         if (rc) {
